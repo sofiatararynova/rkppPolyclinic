66a2381bd37cc501b5c3bdb795df83f4ecf2712e
sofiatararynova <taratynovasofaa@gmail.com>
2026-05-26 23:41:08 +0300

    Таблицы

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module ru.kafpin.rkpppolyclinic {
     requires javafx.controls;
     requires javafx.fxml;
 
+    requires java.sql;
 
     opens ru.kafpin.rkpppolyclinic to javafx.fxml;
     exports ru.kafpin.rkpppolyclinic;
